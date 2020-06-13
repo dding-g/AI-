7c208d81ed2f67adc7a0c89e7cb29591dc136891
ddingg <area409@gmail.com>
2020-06-13 17:10:55 +0900

    DONE dataload:

--- dataset_process/dataloader.m
+++ dataset_process/dataloader.m
@@ -10,16 +10,18 @@ load('./dataset/normal_heartbeat_dataset.mat');
 load('./dataset/murmur_heartbeat_dataset.mat');
 load('./dataset/extrastole_heartbeat_dataset.mat');
 
+%normal_dataset = normal_dataset(:, randperm(size(normal_dataset, 2)));
+%murmur_dataset = murmur_dataset(:, randperm(size(murmur_dataset, 2)));
+%extrastole_dataset = extrastole_dataset(:, randperm(size(normal_dataset, 2)));
 
-Xtrain = [normal_dataset(:, 1:80).'; murmur_dataset(:, 1:35).'; extrastole_dataset(:, 1:25).'];
-Xtest = [normal_dataset(:, 81:115).'; murmur_dataset(:, 36:55).'; extrastole_dataset(:, 26:37).'];
+Xtrain = [normal_dataset(:, 1:184).'; murmur_dataset(:, 1:80).'; extrastole_dataset(:, 1:52).'];
+Xtest = [normal_dataset(:, 184:231).'; murmur_dataset(:, 81:100).'; extrastole_dataset(:, 52:65).'];
 
-% 0 : not normal
 % 1 : normal
-% one-hot encoding
-
-Ytrain = [ones(80, 1) ; ones(60 ,1)*2;];
-Ytest = [ones(35, 1); ones(32,1)*2;];
+% 2 : murmur
+% 3 : extrastole
+Ytrain = [ones(184, 1) ; ones(80 ,1)*2; ones(52 ,1)*3;];
+Ytest = [ones(48, 1); ones(20,1)*2; ones(14,1)*3;];
 
 save './dataset/heartbeat_TrainTestData.mat' Xtrain Xtest Ytrain Ytest
 fprintf('Save complete : heartbeat_TrainTestData.mat\n')
